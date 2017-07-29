a677ee005660b97afe09e63e480948b4a7d8d0c9
juliachen <jchen694@gatech.edu>
2017-07-28 18:16:32 -0700

    Initial Commit

---
+++ FastPitch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FastPitch
+//
+//  Created by Julia Chen on 7/28/17.
+//  Copyright © 2017 Julia Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FastPitch/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FastPitch
+//
+//  Created by Julia Chen on 7/28/17.
+//  Copyright © 2017 Julia Chen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
